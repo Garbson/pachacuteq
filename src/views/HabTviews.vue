52f259de214e058bfcba6e430a5b9528b522dbd4
Garbson <garbsonsouza2602@gmail.com>
2024-01-25 18:38:47 -0500

    hab

--- src/views/HabTviews.vue
+++ src/views/HabTviews.vue
@@ -1,12 +1,12 @@
 <template>
-    <quartoT></quartoT>
-        <contato id="contato"></contato>
-        <Footer></Footer>
-        <Whats></Whats>
-        </template>
-        <script setup>
-         import quartoT from "@/components/quartoT.vue";
-        import contato from "@/components/contato.vue";
-        import Footer from "@/components/Footer.vue";
-        import Whats from "@/components/Whats.vue";
-        </script>
+  <quartoT></quartoT>
+  <contato id="contato"></contato>
+  <Footer></Footer>
+  <Whats></Whats>
+</template>
+<script setup>
+import quartoT from "@/components/quartoT.vue";
+import contato from "@/components/contato.vue";
+import Footer from "@/components/Footer.vue";
+import Whats from "@/components/Whats.vue";
+</script>
