d24e982e3b89f072b7015186a3c0568492aa68e1
Garbson <garbsonsouza2602@gmail.com>
2025-11-03 08:28:14 -0400

    adcionando o icon de instagram

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,15 +4,17 @@
       <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
         <!-- Logo -->
         <div class="a">
-          <img class="a w-[40%] mb-4" src="/img/logo.png" alt="Logo">
+          <img class="a w-[40%] mb-4" src="/img/logo.png" alt="Logo" />
         </div>
 
         <!-- Informações da Empresa -->
         <div>
           <h3 class="text-xl font-semibold mb-4">Sobre Nosotros</h3>
-          <p class="text-quarto-dourado mb-2">Somos una empresa dedicada a la hostelería</p>
+          <p class="text-quarto-dourado mb-2">
+            Somos una empresa dedicada a la hostelería
+          </p>
           <p class="text-quarto-dourado">
-            HOSTAL PACHACUTEQ INN <br>Alameda Pachacuteq N° -Cusco-Perú
+            HOSTAL PACHACUTEQ INN <br />Alameda Pachacuteq N° -Cusco-Perú
           </p>
           <p class="text-quarto-dourado">hostalpachacuteqin@gmail.com</p>
         </div>
@@ -20,16 +22,31 @@
         <!-- Redes Sociais -->
         <div>
           <h3 class="text-xl font-semibold mb-4">Redes Sociales</h3>
-         
-            <a href="#" class="text-quarto-dourado hover:text-quarto-branco transition">
-              <div class="aa"><img class="w-10" src='/img/facebook.png' alt=""></div>
+          <div class="flex flex-row gap-1">
+            <a
+              href="#"
+              class="text-quarto-dourado hover:text-quarto-branco transition"
+            >
+              <div class="aa">
+                <img class="w-10" src="/img/facebook.png" alt="" />
+              </div>
             </a>
-          
+            <a
+              href="#"
+              class="text-quarto-dourado hover:text-quarto-branco transition"
+            >
+              <div class="aa">
+                <img class="w-10" src="/img/instagram.svg" alt="" />
+              </div>
+            </a>
+          </div>
         </div>
       </div>
 
       <!-- Direitos Autorais -->
-      <div class="border-t border-quarto-terracota mt-8 pt-6 text-center text-quarto-dourado">
+      <div
+        class="border-t border-quarto-terracota mt-8 pt-6 text-center text-quarto-dourado"
+      >
         <p>
           &copy; 2024 Hostal Pachacuteq Inn ®. Todos los derechos reservados.
         </p>
@@ -40,14 +57,13 @@
 <style scoped>
 /* Estilos Responsivos */
 @media (max-width: 800px) {
-  footer  {
+  footer {
     text-align: center;
-   
   }
-  .a  { 
-    margin:0 auto ;
+  .a {
+    margin: 0 auto;
   }
-  .aa  { 
+  .aa {
     margin-left: 45%;
     text-align: center;
   }
